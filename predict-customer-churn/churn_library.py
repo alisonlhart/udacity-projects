cce4dd85cfed8f26ab709d9fa9a897a3eda3b6cd
Ali <alhart@redhat.com>
2023-01-29 19:33:18 -0500

    Doing the thing

--- predict-customer-churn/churn_library.py
+++ predict-customer-churn/churn_library.py
@@ -5,6 +5,19 @@
 import os
 os.environ['QT_QPA_PLATFORM']='offscreen'
 
+import pandas as pd
+import numpy as np
+import joblib
+import shap
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, plot_roc_curve
 
 
 def import_data(pth):
@@ -16,7 +29,8 @@ def import_data(pth):
     output:
             df: pandas dataframe
     '''	
-	pass
+    df = pd.read_csv(pth)
+    return df
 
 
 def perform_eda(df):
@@ -28,10 +42,37 @@ def perform_eda(df):
     output:
             None
     '''
-	pass
+    
+    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    
+    plt.figure(figsize=(20,10)) 
+    churn_diagram = df['Churn'].hist()
+    figure = churn_diagram.get_figure()
+    figure.savefig('images/eda/churn_diagram.png')
+        
+    plt.figure(figsize=(20,10)) 
+    age_diagram = df['Customer_Age'].hist()
+    figure = age_diagram.get_figure()
+    figure.savefig('images/eda/age_diagram.png')
+    
+    plt.figure(figsize=(20,10)) 
+    marital_plot = df.Marital_Status.value_counts('normalize').plot(kind='bar')
+    figure = marital_plot.get_figure()
+    figure.savefig('images/eda/marital_plot.png')
+    
+    plt.figure(figsize=(20,10)) 
+    # Show distributions of 'Total_Trans_Ct' and add a smooth curve obtained using a kernel density estimate
+    total_diagram = sns.histplot(df['Total_Trans_Ct'], stat='density', kde=True)
+    figure = total_diagram.get_figure()
+    figure.savefig('images/eda/total_diagram.png')
+    
+    plt.figure(figsize=(20,10)) 
+    heatmap_diagram = sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+    figure = heatmap_diagram.get_figure()
+    figure.savefig('images/eda/heatmap_diagram.png')
 
 
-def encoder_helper(df, category_lst, response):
+def encoder_helper(df, category_lst):
     '''
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
@@ -44,10 +85,20 @@ def encoder_helper(df, category_lst, response):
     output:
             df: pandas dataframe with new columns for
     '''
-    pass
+    for feature in category_lst:
+        lst = []
+        feature_groups = df.groupby(feature).mean()['Churn']
+
+        for val in df[feature]:
+            lst.append(feature_groups.loc[val])
+        name_plus_churn = feature + "_Churn"
+        
+        df[name_plus_churn] = lst   
+    
+    return df
 
 
-def perform_feature_engineering(df, response):
+def perform_feature_engineering(df):
     '''
     input:
               df: pandas dataframe
@@ -59,6 +110,27 @@ def perform_feature_engineering(df, response):
               y_train: y training data
               y_test: y testing data
     '''
+    category_lst = ['Gender', 'Education_Level', 'Marital_Status', 'Income_Category', 'Card_Category']
+    
+    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+            'Total_Relationship_Count', 'Months_Inactive_12_mon',
+            'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+            'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+            'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+            'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
+            'Income_Category_Churn', 'Card_Category_Churn']
+    
+    y = df['Churn']
+    X = pd.DataFrame()
+
+    df = encoder_helper(df, category_lst)
+    
+    X[keep_cols] = df[keep_cols]
+    X.head()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)
+    return X_train, X_test, y_train, y_test, X
+
+
 
 def classification_report_image(y_train,
                                 y_test,
@@ -80,7 +152,25 @@ def classification_report_image(y_train,
     output:
              None
     '''
-    pass
+    plt.clf()
+    plt.rc('figure', figsize=(5, 5))
+    plt.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.axis('off')
+    plt.savefig("images/results/random_forest_classification_report.png")
+    plt.clf()
+    
+    plt.rc('figure', figsize=(5, 5))
+    plt.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
+    plt.axis('off')
+    plt.savefig("images/results/logistic_regression_classification_report.png")
+    plt.clf()
+    
 
 
 def feature_importance_plot(model, X_data, output_pth):
@@ -94,9 +184,30 @@ def feature_importance_plot(model, X_data, output_pth):
     output:
              None
     '''
-    pass
+    importances = model.best_estimator_.feature_importances_
+    # Sort feature importances in descending order
+    indices = np.argsort(importances)[::-1]
 
-def train_models(X_train, X_test, y_train, y_test):
+    # Rearrange feature names so they match the sorted feature importances
+    names = [X_data.columns[i] for i in indices]
+
+    # Create plot
+    plt.figure(figsize=(20,5))
+
+    # Create plot title
+    plt.title("Feature Importance")
+    plt.ylabel('Importance')
+
+    # Add bars
+    plt.bar(range(X_data.shape[1]), importances[indices])
+
+    # Add feature names as x-axis labels
+    plt.xticks(range(X_data.shape[1]), names, rotation=90)
+    plt.savefig(output_pth)
+  
+    
+
+def train_models(X_train, X_test, y_train, y_test, X):
     '''
     train, store model results: images + scores, and store models
     input:
@@ -104,7 +215,84 @@ def train_models(X_train, X_test, y_train, y_test):
               X_test: X testing data
               y_train: y training data
               y_test: y testing data
+              X: pd dataframe 
     output:
               None
     '''
-    pass
+    
+    # grid search
+    rfc = RandomForestClassifier(random_state=42)
+    # Use a different solver if the default 'lbfgs' fails to converge
+    # Reference: https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+    lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
+
+    param_grid = { 
+        'n_estimators': [200, 500],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth' : [4,5,100],
+        'criterion' :['gini', 'entropy']
+    }
+    
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(X_train, y_train)
+
+    lrc.fit(X_train, y_train)
+
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+
+    y_train_preds_lr = lrc.predict(X_train)
+    y_test_preds_lr = lrc.predict(X_test)
+
+    lrc_plot = plot_roc_curve(lrc, X_test, y_test)
+    # plots
+    plt.figure(figsize=(15, 8))
+    ax = plt.gca()
+    rfc_disp = plot_roc_curve(cv_rfc.best_estimator_, X_test, y_test, ax=ax, alpha=0.8)
+    lrc_plot.plot(ax=ax, alpha=0.8)
+    plt.savefig("images/results/roc_lrc_plot.png")
+    plt.show()
+    
+    # save best model
+    joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
+    joblib.dump(lrc, './models/logistic_model.pkl')
+    
+    rfc_model = joblib.load('./models/rfc_model.pkl')
+    lr_model = joblib.load('./models/logistic_model.pkl')
+    
+    lrc_plot = plot_roc_curve(lr_model, X_test, y_test)
+    
+    plt.figure(figsize=(15, 8))
+    ax = plt.gca()
+    rfc_disp = plot_roc_curve(rfc_model, X_test, y_test, ax=ax, alpha=0.8)
+    lrc_plot.plot(ax=ax, alpha=0.8)
+    plt.savefig("images/results/roc_lrc_plot_rfc.png")
+    plt.show()
+
+    
+    explainer = shap.TreeExplainer(cv_rfc.best_estimator_)
+    shap_values = explainer.shap_values(X_test)
+    shap.summary_plot(shap_values, X_test, plot_type="bar")
+    
+    feature_importance_plot(cv_rfc, X, "images/results/feature_importance_plot.png")   
+    classification_report_image(y_train, y_test, y_train_preds_lr, y_train_preds_rf, y_test_preds_lr, y_test_preds_rf)
+
+    
+    
+    
+if __name__ == "__main__":
+    
+    for f in os.listdir("./images/eda"):
+        os.remove(os.path.join("./images/eda", f))
+    for f in os.listdir("images/results"):
+        os.remove(os.path.join("images/results", f))
+    for f in os.listdir("models"):
+        os.remove(os.path.join("models", f))
+    
+    df = import_data(r"./data/bank_data.csv")
+    
+    perform_eda(df)
+    
+    X_train, X_test, y_train, y_test, X = perform_feature_engineering(df)
+    
+    train_models(X_train, X_test, y_train, y_test, X)
--- predict-customer-churn/churn_script_logging_and_tests.py
+++ predict-customer-churn/churn_script_logging_and_tests.py
@@ -1,12 +1,7 @@
 import os
 import logging
-import churn_library_solution as cls
+from churn_library import import_data, perform_eda, encoder_helper, perform_feature_engineering, train_models
 
-logging.basicConfig(
-    filename='./logs/churn_library.log',
-    level = logging.INFO,
-    filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s')
 
 def test_import(import_data):
 	'''
@@ -25,34 +20,123 @@ def test_import(import_data):
 	except AssertionError as err:
 		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
 		raise err
+	return df
 
 
-def test_eda(perform_eda):
+def test_eda(df, perform_eda):
 	'''
 	test perform eda function
 	'''
+	try: 
+		perform_eda(df)
+		assert(os.path.exists('images/eda/churn_diagram.png'))
+		logging.info("Testing test_eda: images/eda/churn_diagram.png exists")
+		assert(os.path.exists('images/eda/age_diagram.png'))
+		logging.info("Testing test_eda: images/eda/age_diagram.png exists")
+		assert(os.path.exists('images/eda/heatmap_diagram.png'))
+		logging.info("Testing test_eda: images/eda/heatmap_diagram.png exists")
+		assert(os.path.exists('images/eda/marital_plot.png'))
+		logging.info("Testing test_eda: images/eda/marital_plot.png exists")
+		assert(os.path.exists('images/eda/total_diagram.png'))
+		logging.info("Testing test_eda: images/eda/total_diagram.png exists")
+		logging.info("Testing perform_eda: SUCCESS")
 
-
-def test_encoder_helper(encoder_helper):
+	except AssertionError as err:
+		logging.error("Testing perform_eda: EDA Diagram is missing.")
+		raise err
+		
+  
+def test_encoder_helper(df, encoder_helper):
 	'''
 	test encoder helper
 	'''
+	category_list =['Gender', 'Education_Level', 'Marital_Status', 'Income_Category', 'Card_Category']
+	try:
+		df = encoder_helper(df, category_list)
+		assert(df)
+		logging.info("Testing test_encoder_helper: df exists")
+		logging.info("Testing test_encoder_helper: SUCCESS")
+	except NameError as err:
+		logging.error("Testing test_encoder_helper: Dataframe wasn't returned from encoder_helper")  
+		raise err
 
 
-def test_perform_feature_engineering(perform_feature_engineering):
+def test_perform_feature_engineering(df, perform_feature_engineering):
 	'''
 	test perform_feature_engineering
 	'''
+	try:
+		X_train, X_test, y_train, y_test, X = perform_feature_engineering(df)
+		assert(X_train.any)
+		logging.info("Testing test_perform_feature_engineering: X_train exists with data")
+		assert(X_test.any)
+		logging.info("Testing test_perform_feature_engineering: X_test exists with data")
+		assert(y_train.any)
+		logging.info("Testing test_perform_feature_engineering: y_train exists with data")
+		assert(y_test.any)
+		logging.info("Testing test_perform_feature_engineering: y_test exists with data")
+		assert(X.any)
+		logging.info("Testing test_perform_feature_engineering: X exists with data")
+		logging.info("Testing test_perform_feature_engineering: SUCCESS")
+	except AssertionError as err:
+		logging.error("Testing test_perform_feature_engineering: Values were not returned.")
+		raise err
+	return X_train, X_test, y_train, y_test, X
 
-
-def test_train_models(train_models):
+def test_train_models(X_train, X_test, y_train, y_test, X, train_models):
 	'''
 	test train_models
 	'''
-
+	try:
+		train_models(X_train, X_test, y_train, y_test, X)
+		assert(os.path.exists("images/results/random_forest_classification_report.png"))
+		logging.info("Testing test_train_models: images/results/random_forest_classification_report.png exists")
+		assert(os.path.exists("images/results/logistic_regression_classification_report.png"))
+		logging.info("Testing test_train_models: images/results/logistic_regression_classification_report.png exists")
+		assert(os.path.exists("images/results/roc_lrc_plot_rfc.png"))
+		logging.info("Testing test_train_models: images/results/roc_lrc_plot_rfc.png exists")
+		assert(os.path.exists("images/results/roc_lrc_plot.png"))
+		logging.info("Testing test_train_models: images/results/roc_lrc_plot.png exists")
+		assert(os.path.exists('./models/rfc_model.pkl'))
+		logging.info("Testing test_train_models: models/rfc_model.pkl exists")
+		assert(os.path.exists('./models/logistic_model.pkl'))
+		logging.info("Testing test_train_models: models/logistic_model.pkl exists")
+		assert(os.path.exists("images/results/feature_importance_plot.png"))
+		logging.info("Testing test_train_models: images/results/feature_importance_plot.png exists")
+		logging.info("Testing test_train_models: SUCCESS")
+	except AssertionError as err:
+		logging.error("Testing test_train_models: Report image not found")
+		raise err
+	
 
 if __name__ == "__main__":
-	pass
+
+	# Clean up images and logs
+	for f in os.listdir("./images/eda"):
+		os.remove(os.path.join("./images/eda", f))
+	for f in os.listdir("images/results"):
+		os.remove(os.path.join("images/results", f))
+	for f in os.listdir("models"):
+		os.remove(os.path.join("models", f))
+	for f in os.listdir("logs"):
+		os.remove(os.path.join("logs", f))  
+  
+	logging.basicConfig(
+	filename='./logs/churn_library.log',
+	level = logging.INFO,
+	filemode='w',
+	format='%(name)s - %(levelname)s - %(message)s')
+ 
+	logging.info("STARTING TESTS") 
+		
+	df = test_import(import_data)
+	test_eda(df, perform_eda)
+	X_train, X_test, y_train, y_test, X = test_perform_feature_engineering(df, perform_feature_engineering)
+	test_train_models(X_train, X_test, y_train, y_test, X, train_models)
+	
+	
+	
+	
 
 
 
